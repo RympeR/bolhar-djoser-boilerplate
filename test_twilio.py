b41dc31f1127d1cd4477c8df52e83b6476fd66fc
George Rashkov <georg.rashkov@gmail.com>
2021-05-28 15:43:33 +0300

    sms reload

--- test_twilio.py
+++ test_twilio.py
@@ -2,13 +2,13 @@ import random
 from twilio.rest import Client
 
 
-acount_sid = 'AC7e26aa22e1d6d1f9439687e1959c6a67'
-acount_token = 'f79382fab3faabc4a162a6d08153637e'
-client = Client(acount_sid,acount_token)
+acount_sid = 'AC3bce9d49f6e044010b3d392f4b9e5921'
+auth_token = '79a1ee2b55bd3b360d2f9260c4cab91f'
+client = Client(acount_sid,auth_token)
 code = random.randint(1,19999)
 phone = "+380953091094"
 message =client.messages.create(
     body=str(code),
-    from_='+13615416379',
+    from_='+12053954148',
     to=phone
 )
