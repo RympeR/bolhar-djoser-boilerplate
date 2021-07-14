f8b81f36539058e4afa3b8a215858fff09432075
George Rashkov <georg.rashkov@gmail.com>
2021-07-14 15:29:20 +0300

    twilio script update

--- apps/utils/func.py
+++ apps/utils/func.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from twilio.rest import Client
 
 acount_sid = 'AC387b7c232d6acb1a2834a84ad56d703d'
-auth_token = '48deda780a9e8bc27f2e833e0badd0a0'
+auth_token = '8ca84c5fa917351e7ee57b7eb115778f'
 
 client = Client(acount_sid, auth_token)
 def set_phone(phone):
